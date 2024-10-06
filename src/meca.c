b1c26a7e121c64da9d9d0034759e3bbffd526aff
crative <hannig.sebi@gmail.com>
2024-10-06 13:42:03 +0200

    asd

--- src/meca.c
+++ src/meca.c
@@ -1,7 +1,23 @@
 #include <ncurses.h>
 
 int main() {
+  initscr();
   raw();
+  keypad(stdscr, TRUE);
   noecho();
+
+  int ch;
+  while (1) {
+    ch = getch();
+    if (ch == KEY_F(1))
+      printw("pressed %c", ch);
+    else {
+      attron(A_BOLD);
+      printw("%c", ch);
+      attroff(A_BOLD);
+    }
+    refresh();
+  }
+  endwin();
   return 0;
 }

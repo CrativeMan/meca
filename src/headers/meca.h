99ad1be3cd906125aa85c9c8b0d23a94cf9739fb
crative <hannig.sebi@gmail.com>
2024-10-06 15:01:07 +0200

    welcome screen

---
+++ src/headers/meca.h
@@ -0,0 +1,6 @@
+#ifndef MECA_H
+#define MECA_H
+
+#define MECA_VERSION "0.1"
+
+#endif // MECA_H
--- src/meca.c
+++ src/meca.c
@@ -1,17 +1,53 @@
+/* imports */
 #include <ncurses.h>
-#include <string.h>
+#include <stdio.h>
 
-int main() {
-  char mesg[] = "Just a string";
-  int row, col;
+#include "headers/meca.h"
+
+/* data structures */
+struct editorSettings {
+  int screencols;
+  int screenrows;
+  int cx, cy;
+};
+struct editorSettings E;
+
+void editorDrawRows() {
+  wmove(stdscr, 0, 0);
+  int i;
+  for (i = 0; i < E.screenrows; i++) {
+    // print welcome message at one third down the screen
+    if (i == E.screenrows/3) {
+      printw("~\n\r");
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "meca editor -- version %s", MECA_VERSION);
+      mvprintw(i, ((E.screencols - welcomelen) / 2)-1, "%s", welcome);
+      printw("\n\r");
+      continue;
+    }
+    printw("~\n\r");
+  }
+  wmove(stdscr, E.cy, E.cx);
+}
+
+/* init */
+void initEditor() {
   initscr();
-  getmaxyx(stdscr, row, col);
   raw();
   keypad(stdscr, TRUE);
+  cbreak();
   noecho();
-  mvprintw(row/2, (col-strlen(mesg))/2, "%s", mesg);
-  mvprintw(row-2, 0, "This screen ahs %d rows and %d columns\n", row, col);
-  printw("Try resizing your window and then run this program again");
+
+  // settings
+  getmaxyx(stdscr, E.screenrows, E.screencols);
+  E.cx = 0;
+  E.cy = 0;
+}
+
+int main() {
+  initEditor();
+  editorDrawRows();
   getch();
   endwin();
   return 0;

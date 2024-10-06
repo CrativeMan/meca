93635540479a49b5d015c8edd9e09bd9976bcd7d
crative <hannig.sebi@gmail.com>
2024-10-06 15:52:37 +0200

    filename stuff lol

--- src/meca.c
+++ src/meca.c
@@ -9,20 +9,30 @@ struct editorSettings {
   int screencols;
   int screenrows;
   int cx, cy;
+  char *filename;
 };
 struct editorSettings E;
 
+/* file i/o */
+void editorOpen(char *filepath) {
+  E.filename = filepath;
+}
+
+/* rendering */
 void editorDrawRows() {
   wmove(stdscr, 0, 0);
   int i;
   for (i = 0; i < E.screenrows; i++) {
     // print welcome message at one third down the screen
-    if (i == E.screenrows/3) {
+    if (i == E.screenrows / 3) {
       printw("~\n\r");
       char welcome[80];
       int welcomelen = snprintf(welcome, sizeof(welcome),
-        "meca editor -- version %s", MECA_VERSION);
-      mvprintw(i, ((E.screencols - welcomelen) / 2)-1, "%s", welcome);
+                                "meca editor -- version %s", MECA_VERSION);
+      if (welcomelen > E.screencols)
+        welcomelen = E.screencols;
+      mvprintw(i, ((E.screencols - welcomelen) / 2) - 1, "%s", welcome);
+      printw("'%s'", E.filename);
       printw("\n\r");
       continue;
     }
@@ -43,10 +53,16 @@ void initEditor() {
   getmaxyx(stdscr, E.screenrows, E.screencols);
   E.cx = 0;
   E.cy = 0;
+  E.filename = NULL;
 }
 
-int main() {
+int main(int argc, char **argv) {
   initEditor();
+
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
+
   editorDrawRows();
   getch();
   endwin();
